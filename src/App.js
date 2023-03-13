c7f85f39e7398a6a12ae1c243b34d6b36310a181
Romain CLEMENT <rc2301@orange.fr>
2023-03-13 10:48:34 +0100

    Manage tasks through App state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import Task from "./Task.js"
 
 class App extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [
+                <Task title="my task 1" checked="" />,
+                <Task title="my task 2" checked="" />,
+                <Task title="my task 3" checked="" />
+            ]
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>To Do List</h1>
-                <Task title="ma tache 1" checked="" />
-                <Task title="ma tache 2" checked="" />
-                <Task title="ma tache 3" checked="" />
+                <ul>{this.state.tasks.map((task) => <li key={task.props.title}>{task}</li>)}</ul>
             </div>
         );
     }
