39caedda87417b82592a1762b653e89161c85e84
Romain CLEMENT <rc2301@orange.fr>
2023-03-20 08:57:10 +0100

    Reformated data storage

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tasks: [
-                <Task title="my task 1" done={false} />,
-                <Task title="my task 2" done={false} />,
-                <Task title="my task 3" done={false} />
-            ]
+            tasks: [],
+            a_i: 0
         }
     }
 
@@ -19,8 +16,14 @@ class App extends React.Component {
         return (
             <div>
                 <h1>To Do List</h1>
-                <ul className='task-list'>{this.state.tasks.map((task) => <li key={task.props.title}>{task}</li>)}</ul>
-                <button onClick={() => this.setState({tasks: this.state.tasks.concat(<Task title={prompt("Title:")} done="" />)})}>New task</button>
+                <ul className='task-list'>{this.state.tasks.map((task) =>
+                    <li key={task.id}><Task id={task.id} title={task.title} checked={task.done} /></li>
+                )}</ul>
+                <button onClick={() => this.setState({tasks: this.state.tasks.concat({
+                    'id': this.state.a_i++,
+                    'title': prompt("Title:"),
+                    'done': false
+                })})}>New task</button>
             </div>
         );
     }
