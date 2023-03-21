5fe32b260e2e87e758b501fcb7cf705680856814
Romain CLEMENT <rc2301@orange.fr>
2023-03-21 22:04:04 +0100

    Resolved deletion error and warnings

--- src/App.js
+++ src/App.js
@@ -14,24 +14,24 @@ class App extends React.Component {
     }
 
     handleCreation() {
-        this.setState({ tasks: { ...this.state.tasks, ...{ [this.state.a_i++]: { 'title': this.state.taskBar, 'done': false } } }, taskBar: '' })
+        this.setState({ tasks: { ...this.state.tasks, ...{ [this.state.a_i]: { 'title': this.state.taskBar, 'done': false } } }, a_i: this.state.a_i + 1, taskBar: '' })
     }
 
-    handleDeletion(task) {
+    handleDeletion(id) {
         let newMap = this.state.tasks;
-        delete newMap[task[0]];
+        delete newMap[id];
         this.setState({ tasks: newMap });
     }
 
-    createRenderedTask(task) {
-        return <li key={task[0]}><Task title={task[1].title} checked={task[1].done} onDelete={() => this.handleDeletion(task[0])} /></li>;
+    createRenderedTask(id, task) {
+        return <li key={id}><Task title={task.title} checked={task.done} onDelete={() => this.handleDeletion(id)} /></li>;
     }
 
     render() {
         return (
             <div>
                 <h1>To Do List</h1>
-                <ul className='task-list'>{Object.entries(this.state.tasks).map((task) => this.createRenderedTask(task))}</ul>
+                <ul className='task-list'>{Object.entries(this.state.tasks).map((entry) => this.createRenderedTask(entry[0], entry[1]))}</ul>
                 <input type="text" placeholder="My new task" value={this.state.taskBar} onChange={(event) => this.setState({ taskBar: event.target.value })} />
                 <button onClick={() => this.handleCreation()}>Add Task</button>
             </div >
