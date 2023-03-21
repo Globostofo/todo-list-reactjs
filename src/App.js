1d8a7dd179678e0610b7d5a236414b9701eed457
Romain CLEMENT <rc2301@orange.fr>
2023-03-21 21:55:49 +0100

    Added textbar to write new task name

--- src/App.js
+++ src/App.js
@@ -8,12 +8,9 @@ class App extends React.Component {
         super(props);
         this.state = {
             tasks: {},
-            a_i: 0
+            a_i: 0,
+            taskBar: ''
         }
-    }
-
-    handleCreation() {
-        this.setState({ tasks: { ...this.state.tasks, ...{ [this.state.a_i++]: { 'title': prompt("Title:"), 'done': false } } } });
     }
 
     handleDeletion(task) {
@@ -31,8 +28,9 @@ class App extends React.Component {
             <div>
                 <h1>To Do List</h1>
                 <ul className='task-list'>{Object.entries(this.state.tasks).map((task) => this.createRenderedTask(task))}</ul>
-                <button onClick={() => this.handleCreation()}>New task</button>
-            </div>
+                <input type="text" placeholder="My new task" value={this.state.taskBar} onChange={(event) => this.setState({ taskBar: event.target.value })} />
+                <button onClick={() => this.setState({ tasks: { ...this.state.tasks, ...{ [this.state.a_i++]: { 'title': this.state.taskBar, 'done': false } } }, taskBar: '' })}>Add Task</button>
+            </div >
         );
     }
 
